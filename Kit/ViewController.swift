fbb71eb1e89bf98296fac3e7078810ca110a5a46
Ilya Yarichevskiy <akva3128@mail.ru>
2022-09-03 17:57:35 +0500

    Change text code

--- Kit/ViewController.swift
+++ Kit/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // MARK: Body
         // Do any additional setup after loading the view.
     }
 
